4dcb6ba8ae56f05d520390d15bab69c51ac043b2
Felipe Weiss <felipe.weiss@proway.treina>
2023-01-12 21:51:17 -0300

    Django

--- alunos/views.py
+++ alunos/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1> Hello World! </h1>')
+    return render(request, 'index.html')
+
+
+def aluno(request):
+    return render(request, 'aluno.html')
 
 # Create your views here.
