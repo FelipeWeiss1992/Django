415d3c2b2fabe8edd41306cbd7194393f3e5d819
Felipe Weiss <felipe.weiss@proway.treina>
2023-01-18 20:15:52 -0300

    django

--- alunos/views.py
+++ alunos/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse
 
 from.models import Alunos
+
 
 def index(request):
     
@@ -12,7 +13,17 @@ def index(request):
     return render(request, 'index.html', dados)
 
 
-def aluno(request):
-    return render(request, 'aluno.html')
+def aluno(request,aluno_id):
+
+    alunos = get_object_or_404(Alunos, pk = aluno_id)
+
+    aluno_a_exibir = {
+
+        'aluno': alunos
+
+    }
+
+    return render(request, 'aluno.html', aluno_a_exibir)
 
 # Create your views here.
+
